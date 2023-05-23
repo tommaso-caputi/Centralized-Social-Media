470d07359188c6c9ab07292a42620e0ad93b72be
tommaso caputi <tommasocaputi85@gmail.com>
2023-05-23 19:12:51 +0200

    add function for call api

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
 window.onload = async () => {
-    
+
+    document.getElementById("login-box");
+
 };
+
+
+const richiestaApi = (json) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    fetch("http://tommasocaputi.altervista.org/CSM/webhook.php", {
+        method: 'POST',
+        headers: myHeaders,
+        body: JSON.stringify(json),
+        redirect: 'follow'
+    })
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
